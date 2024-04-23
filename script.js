576b96483a4e1e08b33afd89b57296c844135fb2
Prasanna <pgurung1@my.hpu.edu>
2024-04-23 16:29:29 -0500

    added getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello world")
+
+function getComputerChoice(numchoice){
+    var choice= parseInt(Math.random()*numchoice);
+    if (choice==0){
+        return "Rock";
+    }
+    else if (choice==1){
+        return "Scissor";
+    }
+    else{
+        return "Paper";
+    }
+       
+}
+console.log(getComputerChoice(3))
